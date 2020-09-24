311af397e5b439b518d066b24326de3045d83ccd
Vlad Huliaiev <gulyaev4420@gmail.com>
2020-09-24 16:41:11 +0300

    Updates

---
+++ precache-manifest.d42765cf040993a4d96690a13ddd7645.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31d8f6d567d88c1417e92ed6f831ed2",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/index.html"
+  },
+  {
+    "revision": "64c98374b57eb098dc25",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/css/main.75eb1570.chunk.css"
+  },
+  {
+    "revision": "8396aead4d31091d8e6c",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/js/2.adabc07a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/js/2.adabc07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64c98374b57eb098dc25",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/js/main.6608ccc5.chunk.js"
+  },
+  {
+    "revision": "655d5afdb46742fde920",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/js/runtime-main.a9adb6d3.js"
+  },
+  {
+    "revision": "50b2c27928cb7e62eb9a154818055d27",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/1.50b2c279.jpg"
+  },
+  {
+    "revision": "b0b32625182cd9dde75690aa2178be49",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/10.b0b32625.jpg"
+  },
+  {
+    "revision": "36701627227b7dc7ae1b1e4f7be6435d",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/11.36701627.jpg"
+  },
+  {
+    "revision": "aa57001e555fbb961bcc45e5a44425ab",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/12.aa57001e.jpg"
+  },
+  {
+    "revision": "7abd513db3bbe42b4978b054dfb71301",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/13.7abd513d.jpg"
+  },
+  {
+    "revision": "5843a696278bcb23082f41b796946b9c",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/14.5843a696.jpg"
+  },
+  {
+    "revision": "e2d8641b6d5d5f4e3e421cc288f03171",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/15.e2d8641b.jpg"
+  },
+  {
+    "revision": "0ea38171aa78e7c42e263b58b24088a8",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/16.0ea38171.jpg"
+  },
+  {
+    "revision": "35d82be1b1e17699b7685b6661aeee63",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/17.35d82be1.jpg"
+  },
+  {
+    "revision": "fe2c5bbdfb071f45e597de4b899b78c2",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/18.fe2c5bbd.jpg"
+  },
+  {
+    "revision": "b88c3b4b8de7b48ebb68d1683e769c69",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/19.b88c3b4b.jpg"
+  },
+  {
+    "revision": "ff5a5bb3731a525a76e762cee029120d",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/2.ff5a5bb3.jpg"
+  },
+  {
+    "revision": "88eff8fe2fcf99eda3d51cc671eb27d9",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/20.88eff8fe.jpg"
+  },
+  {
+    "revision": "f92275fa1afefed81480eb7b3983fdb8",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/3.f92275fa.jpg"
+  },
+  {
+    "revision": "1bb4acbe26ac2bf21fc1a1869ef7cff1",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/4.1bb4acbe.jpg"
+  },
+  {
+    "revision": "cef647af0acddba7dc96e3c6b61bec14",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/5.cef647af.jpg"
+  },
+  {
+    "revision": "4785438d4ec3376904583cd57e20894b",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/6.4785438d.jpg"
+  },
+  {
+    "revision": "c76db3722545aac5a210628c7ab67340",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/7.c76db372.jpg"
+  },
+  {
+    "revision": "a6d0038d9ae2f8eb5d83ee41f7c80b89",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/8.a6d0038d.jpg"
+  },
+  {
+    "revision": "16e3a5f4976783b2b15de998093964f3",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/9.16e3a5f4.jpg"
+  },
+  {
+    "revision": "18867fcd477d88985ae98d33173185b6",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/anakin_img.18867fcd.jpg"
+  },
+  {
+    "revision": "b189fb6ba69834a5c067896c34f52080",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/background-start.b189fb6b.jpg"
+  },
+  {
+    "revision": "3352fdeb8e3413067df241dbfdcd4031",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/leia_img.3352fdeb.jpg"
+  },
+  {
+    "revision": "55da1bf0166a5eac98ee67e20a1ac573",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/luke_img.55da1bf0.jpg"
+  },
+  {
+    "revision": "847fcd22ff976ede1957c34e4eb3baa6",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/mandalorian.847fcd22.jpg"
+  },
+  {
+    "revision": "40b50c9e452e56b79e8bff1241aa3d54",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/palpatine.40b50c9e.jpg"
+  },
+  {
+    "revision": "22de823b055e4f6b460f6a86c66042e8",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/rey_avatar.22de823b.jpg"
+  },
+  {
+    "revision": "50847147924e88d4d429dc887b29341a",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/rey_img.50847147.jpg"
+  },
+  {
+    "revision": "718524087d6534674e0793834348b97b",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/rey_img_2.71852408.jpg"
+  },
+  {
+    "revision": "28584aa096f1429d5c4d19af7957e630",
+    "url": "/ReactJS.-Concept-of-SPA-Navigation/static/media/rey_img_3.28584aa0.jpg"
+  }
+]);
