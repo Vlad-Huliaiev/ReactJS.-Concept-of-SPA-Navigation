0bc932e85e2ad19c4bbd3d61f7a4d872625ee009
Vlad Huliaiev <gulyaev4420@gmail.com>
2020-09-26 22:49:08 +0300

    fix v0.5

--- src/App.js
+++ src/App.js
@@ -2,31 +2,30 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 
 import Header from "./header/header.js";
-import start from "./start/start.js";
-import photo from "./pin/renderPin.js";
-import posts from './posts/posts.js';
-import contacts from './contacts/contacts.js';
-
+import Start from "./start/start.js";
+import Photo from "./pin/renderPin.js";
+import Posts from './posts/posts.js';
+import Contacts from './contacts/contacts.js';
 
 export const Menu = [
     {
         title: "start",
         path: "start",
-        component: start ,
+        component: Start ,
     },{
         title: "photo",
         path: "photo",
-        component: photo,
+        component: Photo,
         exact: true
     },{
         title: "posts",
         path: "posts",
-        component: posts,
+        component: Posts,
         exact: true
     },{
         title: "contacts",
         path: "contacts",
-        component: contacts,
+        component: Contacts,
         exact: true
     }
 ];
@@ -39,10 +38,18 @@ function App() {
                 <Header />
                 <Switch>
                     <Route path="/" exact render={() => <Redirect to="/start"/>}/>
-                    <Route exact path='/start' component={start}/>
-                    <Route exact path='/photo' component={photo}/>
-                    <Route exact path='/posts' component={posts}/>
-                    <Route exact path='/contacts' component={contacts}/>
+                    <Route exact path='/start'>
+                        <Start />
+                    </Route>
+                    <Route exact path='/photo'>
+                        <Photo />
+                    </Route>
+                    <Route exact path='/posts'>
+                        <Posts />
+                    </Route>
+                    <Route exact path='/contacts'>
+                        <Contacts />
+                    </Route>
                 </Switch>
             </Router>
         </div>
